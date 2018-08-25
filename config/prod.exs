e6ccc922a719f1575cea444d795fa911ddf6bdd4
Isaac Sanders <isaac@isaacbfsanders.com>
2018-08-25 15:02:13 -0500

    Revert "Updates URL"
    This reverts commit 3e324c368edf5900c23c1810ed3a71bb3ff06bcb.

--- config/prod.exs
+++ config/prod.exs
@@ -2,7 +2,7 @@ use Mix.Config
 
 config :game_of_life, GameOfLifeWeb.Endpoint,
   load_from_system_env: true,
-  url: [scheme: "http", host: "game-of-life.isaacbfsanders.com", port: 80],
+  url: [scheme: "https", host: "game-of-life.isaacbfsanders.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
